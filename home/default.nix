40763eea9674bedecd29fcd05e948e860506ceb3
微笑 <wang1234561211@outlook.com>
2024-12-27 22:43:46 +0800

    feat: add lazygit config

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,7 @@
     # ./git.nix
     ./starship.nix
     ./yazi.nix
+    ./lazygit.nix
   ];
 
   # Home Manager needs a bit of information about you and the
---
+++ home/lazygit.nix
@@ -0,0 +1,29 @@
+{
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui = {
+        language = "zh-CN";
+        nerdFontsVersion = "3";
+        commitLength = {
+          show = true;
+        };
+        mainPanelSplitMode = "flexible";
+        timeFormat = "2006-01-02 15:04:05";
+        shortTimeFormat = "15:04";
+      };
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "delta --dark --paging=never --syntax-theme base16-256 --diff-so-fancy";
+        };
+      };
+      notARepository = "skip";
+      startupPopupVersion = 1;
+      reporting = "off";
+    };
+  };
+}
+
+
+
