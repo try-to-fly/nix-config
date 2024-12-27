016c72877eb1444d8bda894f1ccac466736ecdef
微笑 <wang1234561211@outlook.com>
2024-12-27 20:30:19 +0800

    feat: add starship config

--- home/default.nix
+++ home/default.nix
@@ -6,7 +6,7 @@
     # ./shell.nix
     # ./core.nix
     # ./git.nix
-    # ./starship.nix
+    ./starship.nix
   ];
 
   # Home Manager needs a bit of information about you and the
---
+++ home/starship.nix
@@ -0,0 +1,29 @@
+{ ... }: {
+  programs.starship = {
+    enable = true;
+
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableNushellIntegration = true;
+
+    settings = {
+      battery = {
+        full_symbol = "🔋 ";
+        charging_symbol = "⚡️ ";
+        discharging_symbol = "💀 ";
+      };
+      git_commit = {
+        commit_hash_length = 6;
+        tag_symbol = "🔖 ";
+      };
+      git_metrics = {
+        added_style = "bold blue";
+        format = "[+$added]($added_style)/[-$deleted]($deleted_style) ";
+      };
+      git_state = {
+        format = "[\($state( $progress_current of $progress_total)\)]($style) ";
+        cherry_pick = "[🍒 PICKING](bold red)";
+      };
+    };
+  };
+}
