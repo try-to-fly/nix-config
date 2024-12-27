0d7002d293bffe52e696643867cff10dbcc88a41
微笑 <wang1234561211@outlook.com>
2024-12-27 13:18:10 +0800

    feat: update nix core

--- modules/nix-core.nix
+++ modules/nix-core.nix
@@ -1,22 +1,30 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  nix.settings = {
-    # enable flakes globally
-    experimental-features = [ "nix-command" "flakes" ];
+  # enable flakes globally
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-    # substituers that will be considered before the official ones(https://cache.nixos.org)
-    substituters = [
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    builders-use-substitutes = true;
-  };
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
+  # Use this instead of services.nix-daemon.enable if you
+  # don't wan't the daemon service to be managed for you.
+  # nix.useDaemon = true;
+
   nix.package = pkgs.nix;
+
+  # do garbage collection weekly to keep disk usage low
+  nix.gc = {
+    automatic = lib.mkDefault true;
+    options = lib.mkDefault "--delete-older-than 7d";
+  };
+
+  # Disable auto-optimise-store because of this issue:
+  #   https://github.com/NixOS/nix/issues/7273
+  # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
+  nix.settings = {
+    auto-optimise-store = false;
+  };
 }
