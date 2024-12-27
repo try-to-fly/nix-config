89a267405295a39c92227788716aec08398e2899
微笑 <wang1234561211@outlook.com>
2024-12-27 20:51:34 +0800

    feat: add yazi config

--- home/default.nix
+++ home/default.nix
@@ -7,6 +7,7 @@
     # ./core.nix
     # ./git.nix
     ./starship.nix
+    ./yazi.nix
   ];
 
   # Home Manager needs a bit of information about you and the
---
+++ home/yazi.nix
@@ -0,0 +1,14 @@
+{
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      manager = {
+        show_hidden = true;
+        sort_dir_first = true;
+        linemode = "owner";
+      };
+
+    };
+  };
+}
