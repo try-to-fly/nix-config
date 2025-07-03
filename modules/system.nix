89920476b5513c586fcf6f5044c1578b5e31d595
smile <wang1234561211@outlook.com>
2025-07-03 17:46:09 +0800

    fix: nix update error

--- modules/system.nix
+++ modules/system.nix
@@ -11,19 +11,14 @@
 {
   system = {
     stateVersion = 5;
-    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postUserActivation.text = ''
-      # activateSettings -u will reload the settings from the database and apply them to the current session,
-      # so we do not need to logout and login again to make the changes take effect.
-      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-    '';
-
     defaults = {
       menuExtraClock.Show24Hour = true; # show 24 hour clock
 
       # other macOS's defaults configuration.
       # ......
     };
+
+    primaryUser = "smile";
   };
 
   environment = {
